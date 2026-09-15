ebe515ebb3f4c9918f62c199f6c9e927557cb126
rohneg <rnegi7689@gmail.com>
2026-09-15 22:05:41 +0530

    Added file handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,34 @@
 #include <iostream>
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
 
 int main() {
+    std::string filePath;
+
     std::cout << "=================================\n";
-    std::cout << "      ChunkVault Started\n";
+    std::cout << "          ChunkVault\n";
     std::cout << "=================================\n";
-    std::cout << "Fault-Tolerant File Storage System\n";
-    std::cout << "System setup successful!\n";
+
+    std::cout << "Enter file path: ";
+    std::getline(std::cin, filePath);
+
+    if (!fs::exists(filePath)) {
+        std::cout << "\nError: File not found.\n";
+        return 1;
+    }
+
+    if (!fs::is_regular_file(filePath)) {
+        std::cout << "\nError: Selected path is not a file.\n";
+        return 1;
+    }
+
+    fs::path path(filePath);
+
+    std::cout << "\nFile found successfully!\n";
+    std::cout << "File name: " << path.filename() << '\n';
+    std::cout << "File size: " << fs::file_size(path) << " bytes\n";
 
     return 0;
 }
