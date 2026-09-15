0f43013a3bf746668c0fbe67cd10689467a9a285
rohneg <rnegi7689@gmail.com>
2026-09-15 21:58:16 +0530

    Initial project setup

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "=================================\n";
+    std::cout << "      ChunkVault Started\n";
+    std::cout << "=================================\n";
+    std::cout << "Fault-Tolerant File Storage System\n";
+    std::cout << "System setup successful!\n";
+
+    return 0;
+}
